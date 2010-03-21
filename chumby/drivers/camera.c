4984876e4747baae48c4cabd8e799ea40ada924e
shollwed@ucsd.edu <shollwed@ucsd.edu@517cf41e-c4f2-11de-afa3-455a29e9879b>
2010-03-21 21:27:32 +0000

    Camera driver is mostly working

--- chumby/drivers/camera.c
+++ chumby/drivers/camera.c
@@ -8,27 +8,40 @@
 int initialize_camera()
 {
 	struct termios camera_term;
+	char buffer[5];
 
 	// Initialize serial to camera
-	int tty = open("/dev/camera", O_RDWR | O_NOCTTY | O_NDELAY);
+	int tty = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NONBLOCK);
    	tcgetattr(tty, &camera_term);
-	cfmakeraw(&camera_term);
+	//cfmakeraw(&camera_term);
   	camera_term.c_cflag = B115200;
-   	camera_term.c_cflag = CS8 | CREAD | CLOCAL;
-   	camera_term.c_iflag = IGNPAR | IGNBRK;
-	tcflush(tty, TCOFLUSH);
+   	//camera_term.c_cflag = CS8 | CREAD | CLOCAL;
+   	//camera_term.c_iflag = IGNPAR | IGNBRK;
 	tcsetattr(tty,TCSANOW,&camera_term);
+	usleep(50000);
 
 	//Set tracking settings
 	char tc[] = "om 0 0\r"; //Suppress output from tracking
    	char servo[] = "sm 5\r";//Set to pan servo active and pan servo report
-	char color_track[] = "tc 144 194 37 87 0 41\r";
-	write(tty,&tc,sizeof(tc));
-	rx_ack(tty);
-	write(tty,&servo,sizeof(servo));
-	rx_ack(tty);
-	write(tty, &color_track,sizeof(color_track));
-	rx_ack(tty);
+	char color_track[] = "st 144 194 37 87 0 41\r"; //Set color tracking settings
+	
+	do {
+		write(tty,&tc,sizeof(tc));
+		usleep(50000);
+		read(tty, buffer, 5);
+	} while (buffer[0] != 'A');
+	
+	do {
+		write(tty,&servo,sizeof(servo));
+		usleep(50000);
+		read(tty, buffer, 5);
+	} while (buffer[0] != 'A');
+	
+	do {
+		write(tty, &color_track,sizeof(color_track));
+		usleep(50000);
+		read(tty, buffer, 5);
+	}while(buffer[0] != 'A');
 
 	return tty;
 }
@@ -54,7 +67,6 @@ void start_tracking(int tty)
 {
 	char servo[] = "so 0 1\r";//Turn on Servo
 	write(tty,&servo,sizeof(servo));
-	rx_ack(tty);
 }
 
 //Turn off servos
@@ -62,7 +74,6 @@ void stop_tracking(int tty)
 {
 	char servo[] = "so 0 0\r";//Turn off Servo
 	write(tty,&servo,sizeof(servo));
-	rx_ack(tty);
 }
 
 
@@ -74,12 +85,29 @@ int servo_position(int tty)
 	int position = 0; 
    	char get_servo_pos[] = "gs 0\r"; //Get position of servo 0
 	write(tty,&get_servo_pos,sizeof(get_servo_pos));
-	for(count = 0; count < sizeof(buffer);count++)
+	usleep(50000);
+	read(tty,buffer,9);
+	if(buffer[6] == 13)
 	{
-		read(tty,&buffer[count],1);
+		position = (buffer[4] - 48)*10 + buffer[5] - 48;
 	}
-
-	position = (buffer[5] - 48)*100 + (buffer[6] - 48)*10 + (buffer[7] - 48);
-
+	else
+	{
+		position = (buffer[4] - 48)*100 + (buffer[5] - 48)*10 + (buffer[6] - 48);
+	}
+	
 	return position;
+}
+
+int main()
+{
+	int tty = initialize_camera();
+	printf("%d\n",servo_position(tty));
+	//char buffer[] = "sv 0 90\r";
+	//char input[5];
+	//write(tty, buffer, sizeof(buffer));
+	//usleep(50000);
+	//read(tty,input,5);
+	close(tty);
+	return 0;
 }
