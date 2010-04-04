e1ab67f240030bb43b03182afd2b0c981a8e83cd
mtortiz.mail <mtortiz.mail@517cf41e-c4f2-11de-afa3-455a29e9879b>
2010-04-04 22:07:17 +0000

    Initial work on the main control app.

--- chumby/demos/object avoidance demo/sonar.c
+++ chumby/demos/object avoidance demo/sonar.c
@@ -1,9 +1,9 @@
-/*	A module that has functions that will read from the
- *	specified sonar sensor, the distance from that that
- *	sensor to the nearest object in inches.
+/*      A module that has functions that will read from the
+ *      specified sonar sensor, the distance from that that
+ *      sensor to the nearest object in inches.
  *
- *  Author:	Michael Ortiz (mtortiz.mail@gmail.com)
- *  Modified:	Scott Hollwedel (s.hollwedel@gmail.com)(1/22/10)
+ *  Author:     Michael Ortiz (mtortiz.mail@gmail.com)
+ *  Modified:   Scott Hollwedel (s.hollwedel@gmail.com)(1/22/10)
  */
 
 #include "sonar.h"
@@ -22,67 +22,67 @@
 
 void initialize_sonar(int tty)
 {
-	int address = SONAR_FIRST_ADDR; //Address of 1st sensor}
-	int output = 0;
+        int address = SONAR_FIRST_ADDR; //Address of 1st sensor}
+        int output = 0;
 
 //Make sure everything works, in this case read the software version #
-	while ( address <= SONAR_LAST_ADDR )
-	{
-		write_i2c(tty, address + 1, 0, 1, 0);
-		read(tty,&output,1);
-		address = address + 2;
-		if(output != 10)
-		{
-			fprintf(stderr, "Sonar is broken.\n");
-			return -1;
-		}
-	}
+        while ( address <= SONAR_LAST_ADDR )
+        {
+                write_i2c(tty, address + 1, 0, 1, 0);
+                read(tty,&output,1);
+                address = address + 2;
+                if(output != 10)
+                {
+                        fprintf(stderr, "Sonar is broken.\n");
+                        return;
+                }
+        }
 
-	// Set new range(6m)
-	address = SONAR_FIRST_ADDR;
-	while ( address <= SONAR_LAST_ADDR )
-	{
-		write_i2c(tty, address, 2, 1,RANGE);
-		read(tty,&output,1);
-		address= address + 2;
-	}
+        // Set new range(6m)
+        address = SONAR_FIRST_ADDR;
+        while ( address <= SONAR_LAST_ADDR )
+        {
+                write_i2c(tty, address, 2, 1,RANGE);
+                read(tty,&output,1);
+                address= address + 2;
+        }
 
-	// Set new gain corresponding to range
-	address = SONAR_FIRST_ADDR;
-	while ( address <= SONAR_LAST_ADDR )
-	{
-		write_i2c(tty, address, 1, 1,GAIN);
-		read(tty,&output,1);
-		address = address + 2;
-	}
+        // Set new gain corresponding to range
+        address = SONAR_FIRST_ADDR;
+        while ( address <= SONAR_LAST_ADDR )
+        {
+                write_i2c(tty, address, 1, 1,GAIN);
+                read(tty,&output,1);
+                address = address + 2;
+        }
 }
 
 void take_range( int tty )
 {
-	int address = SONAR_FIRST_ADDR; //Address of 1st sensor
-	int output = 0;
-	// Take range
-	while ( address <= SONAR_LAST_ADDR )
-	{
-		write_i2c(tty, address, 0, 1,80);
-		read(tty, &output, 1);
-		address = address + 2;
-		usleep(100000); //Delay for sent pulses, can be removed if sonar facing in different directions
-	}
+        int address = SONAR_FIRST_ADDR; //Address of 1st sensor
+        int output = 0;
+        // Take range
+        while ( address <= SONAR_LAST_ADDR )
+        {
+                write_i2c(tty, address, 0, 1,80);
+                read(tty, &output, 1);
+                address = address + 2;
+                usleep(100000); //Delay for sent pulses, can be removed if sonar facing in different directions
+        }
 }
 
 int get_front_range(int tty)
 {
-	int output = 0;
-	write_i2c(tty, 0xE1, 3, 1, 0);
-	read(tty,&output,1);
-	return output;
+        int output = 0;
+        write_i2c(tty, 0xE1, 3, 1, 0);
+        read(tty,&output,1);
+        return output;
 } 
 
 int get_right_range( int tty)
 {
-	int output = 0;
-	write_i2c(tty, 0xE3, 3, 1, 0);
-	read(tty, &output,1);
-	return output;
+        int output = 0;
+        write_i2c(tty, 0xE3, 3, 1, 0);
+        read(tty, &output,1);
+        return output;
 }
---
+++ chumby/main_app/main.h
@@ -0,0 +1,32 @@
+/*  Author: Michael Oritz
+ *  Email: mtortiz.mail@gmail.com
+ *  Date: 4/1/2010
+ *
+ *  main.h
+ *  This file contains sensory data and state machine info that is global
+ *  all robomagellan control modules.
+ */
+
+struct {
+    double compass_heading; // compass heading in degrees
+    double target_bearing;  // bearing to target in degress
+    double target_distance;  // distance to target in meters
+    int front_sonar;    // distance from front sonar to object in cm
+    int right_sonar;    // distance from right sonar to object in cm
+    int speed;  // -100 reverse, 0 stop, +100 forward
+    int turn;   // -100 left, 0 center, +100 forward
+} sensory_data;
+
+enum {
+    INIT_STATE = 0,
+    NAVIGATION_STATE,
+    OBJECT_AVOIDANCE_STATE,
+    TRACK_CONE_STATE,
+    DONE_STATE
+} STATES;
+
+int current_state;
+int next_state;
+
+int gps_tty;
+int compass_tty;
