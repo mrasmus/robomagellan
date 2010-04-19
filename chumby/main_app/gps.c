f301c201269c833d7c89c0a00747cad474ad942e
shollwed@ucsd.edu <shollwed@ucsd.edu@517cf41e-c4f2-11de-afa3-455a29e9879b>
2010-04-19 05:23:39 +0000

    GPS code brought to working state, should be ready (untested on Chumby).

--- chumby/main_app/gps.c
+++ chumby/main_app/gps.c
@@ -1,8 +1,8 @@
 //GPS NMEA Driver + Comparitive positioning computation
-////    Written by Matthew Rasmussen, 2010
-////    Released under GPL license.
-////            !!When compiling for Chumby, use Lib Math ("-lm" flag) or       !!
-////            !!it won't work!                                                !!
+////	Written by Matthew Rasmussen, 2010
+////	Released under GPL license.
+////			!!When compiling for Chumby, use Lib Math ("-lm" flag) or	   !!
+////			!!it won't work!												!!
 
 #include <termios.h>
 #include <stdio.h>
@@ -16,9 +16,9 @@
 #define PI 3.14159265358979
 //Radius in meters:
 
-#define R               6378000
-#define DtoR    (PI / 180)
-#define RtoD    (180 / PI)
+#define R		6378000
+#define DtoR	(PI / 180)
+#define RtoD	(180 / PI)
 
 static int tty;
 
@@ -31,7 +31,7 @@ int gps_init()
   struct termios gps_term;
 
   //tty = open("/dev/gps", O_RDWR | O_NOCTTY);
-  tty = open("/dev/tty.PL2303-0000201A", O_RDWR | O_NOCTTY);
+  tty = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NONBLOCK);
   tcgetattr(tty, &gps_term);
   cfmakeraw(&gps_term);
   cfsetspeed(&gps_term, B4800);
@@ -42,125 +42,135 @@ int gps_init()
   gps_term.c_cc[VMIN] = 0; 
   tcsetattr(tty,TCSAFLUSH,&gps_term);
 
-  fprintf(stderr,"tty opened:%d\n",tty);
   return tty;
 }
 
 double convert_nmea_ll(char * lat)
 {
-    int degrees = 0;
-    double minutes;
+	double degrees = 0;
+	double minutes = 0;
 
-    int count;
-    int dot = 0;
+	int count;
+	int dot = 0;
 	double a = 1;
+	char direction = 0;
 
-    while (lat[dot] != '.')
-    {
-        dot++;
-    }
-
-    for ( count = 0; count < dot - 2; count++ )
-    {
-        degrees *= 10;
-        degrees += lat[count] - '0';
-    }
-
-    minutes = (10 * (lat[dot - 2] - '0')) + (lat[dot - 1] - '0');
-	for (count = dot + 1; lat[count] <= '9' && lat[count] >= '0'; count++)
+	while (lat[dot] != '.')
 	{
-		a *= 0.1;
-		minutes += (lat[count] - '0') * a;
+		dot++;
 	}
 
-    return (degrees + (minutes / 60)) * ((lat[count+1] == 'W' || lat[count+1] == 'S') ? -1 : 1);
+	for ( count = 0; count < dot - 2; count++ )
+	{
+		degrees *= 10;
+		degrees += lat[count] - '0';
+	}
+
+	sscanf(&(lat[count]),"%lf,%c",&minutes,&direction);
+
+	degrees = (degrees + (minutes / 60)) * ((direction == 'W' || direction == 'S') ? -1 : 1);
+	return degrees;
 }
 
 //Read GPS coordinates and return lat and long - data is in NMEA 0183 format
 int gps_get_position(struct Location* position)
 {
-    unsigned char buf[64]; //Fill up with each line
-    unsigned char latitude[11];
-    unsigned char longitude[12];
-    int found = 0;
+	unsigned char buf[64]; //Fill up with each line
+	unsigned char latitude[12];
+	unsigned char longitude[13];
+	int found = 0;
 	int retries = 0;
-	int messages = 0;
-    
-    //Disregard data until $GPRMC is found and is valid
-    while( found != 1)
-    {
+	int validity;
+
+	latitude[11] = 0;
+	longitude[12] = 0;
+	
+	//Disregard data until $GPRMC is found and is valid
+	while( found != 1)
+	{
 		while (read(tty, &buf[0], 64) <= 0)
 		{
-			fprintf(stderr,"Waiting on successful read, retries: %d\n",retries);
 			usleep(5000);
 			if(retries++ > 50)
-				return 0;
+				return -1;
 		}
 
-		fprintf(stderr,"%d messages processed, found: %d\n",messages,found);
-        if(strncmp((char *)buf, "$GPRMC", 6) == 0)
-        {
-            //Read in GPS data and get lat and long
-            found = 1;
-            if(buf[18] == 'V');
-                found = 0;
-            memcpy(latitude, &buf[20],11);
-            memcpy(longitude,&buf[32],12);
-        }
-		messages++;
-    }
-    position->latitude = convert_nmea_ll((char *)latitude);
-    position->longitude = convert_nmea_ll((char *)longitude);
+		if(strncmp((char *)buf, "$GPRMC", 6) == 0)
+		{
+			//Read in GPS data and get lat and long
+			found = 1;
+			buf[63] = 0;
+			fprintf(stderr,"\nString: %s\n\n",buf);
+			memcpy(latitude, &buf[20],11);
+			memcpy(longitude,&buf[32],12);
+			if(buf[18] == 'A')
+			{
+				//Data from GPS was formatted correctly, but not trustworthy
+				//(inValid), likely satellites could not be seen.
+				validity = 0;
+			}
+			else //Data being returned is Active (valid/trustworthy)
+				validity = 1;
+		}
+	}
+	position->latitude = convert_nmea_ll((char *)latitude);
+	position->longitude = convert_nmea_ll((char *)longitude);
 
-	return 1;
+	return validity;
 }
 
 double calc_target_distance(struct Location* pos, struct Location* dest)
 {
-    double a1,b1,a2,b2,dist;
+	double a1,b1,a2,b2,dist;
 
-        a1 = pos->latitude * DtoR;
-        b1 = pos->longitude * DtoR;
-        a2 = dest->latitude * DtoR;
-        b2 = dest->longitude * DtoR;
+		a1 = pos->latitude * DtoR;
+		b1 = pos->longitude * DtoR;
+		a2 = dest->latitude * DtoR;
+		b2 = dest->longitude * DtoR;
 
-        dist = acos((cos(a1) * cos(b1) * cos(a2) * cos(b2)) + (cos(a1) * sin(b1) * cos(a2) * sin(b2)) + (sin(a1) * sin(a2))) * R;
+		dist = acos((cos(a1) * cos(b1) * cos(a2) * cos(b2)) + (cos(a1) * sin(b1) * cos(a2) * sin(b2)) + (sin(a1) * sin(a2))) * R;
 
-        printf("a1: %f\nb1: %f\na2: %f\nb2: %f\n",a1,b1,a2,b2);
-    printf("distance: %f meters.\n",dist);
+		printf("a1: %f\nb1: %f\na2: %f\nb2: %f\n",a1,b1,a2,b2);
+	printf("distance: %f meters.\n",dist);
 
-    return dist;
+	return dist;
 }
 
 double calc_target_heading(struct Location* pos, struct Location* dest)
 {
-    double dlong, lat1, lat2, y, x, bearing;
+	double dlong, lat1, lat2, y, x, bearing;
 
-        lat1 = pos->latitude * DtoR;
-        lat2 = dest->latitude * DtoR;
-        dlong = (dest->longitude - pos->longitude) * DtoR;
-        
-        y = sin(dlong) * cos(lat2);
-        x = (cos(lat1) * sin(lat2)) - (sin(lat1) * cos(lat2) * cos(dlong));
-        bearing = atan2(y,x) * RtoD;
-        bearing = (bearing < 0) ? bearing + 360 : bearing;
+	lat1 = pos->latitude * DtoR;
+	lat2 = dest->latitude * DtoR;
+	dlong = (dest->longitude - pos->longitude) * DtoR;
+		
+	y = sin(dlong) * cos(lat2);
+	x = (cos(lat1) * sin(lat2)) - (sin(lat1) * cos(lat2) * cos(dlong));
+	bearing = atan2(y,x) * RtoD;
+	bearing = (bearing < 0) ? bearing + 360 : bearing;
 
-    return bearing;
+	return bearing;
 }
 
-#if 1
+#if 0
 main()
 {
-        struct Location pos, dest;
-        dest.latitude = 32.881136091348665;
-        dest.longitude = -117.23563715815544;
+	struct Location pos, dest;
+	//Test values, at the Geisel end of Warren Mall.
+	dest.latitude = 32.881136091348665;
+	dest.longitude = -117.23563715815544;
 
-        while (1)
-        {
-			if (gps_get_position(&pos) < 1)
-				printf("Error reading.\n");
-            gps_get_position(&pos);
-            printf("From %f,%f to %f,%f: %f  at heading %f\n",pos.latitude,pos.longitude,dest.latitude,dest.longitude,calc_target_distance(&pos,&dest),calc_target_heading(&pos,&dest));
-        }
+	//Uncomment to spoof NMEA Strings on stdio
+	//tty = 0;
+
+	while(gps_init() < 1)
+	{}
+
+	while (1)
+	{
+		if (gps_get_position(&pos) < 1)
+			printf("Data inValid.\n");
+		printf("From %f,%f to %f,%f: %f  at heading %f\n",pos.latitude,pos.longitude,dest.latitude,dest.longitude,calc_target_distance(&pos,&dest),calc_target_heading(&pos,&dest));
+	}
 }
 #endif
